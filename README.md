20c4cfb84ee2f1e0e6d121c79978f553a233bd3d
Kamran Ali <kamranalinitb@gmail.com>
2018-03-02 22:05:45 +0530

    Displaying article in frontend

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Chainlist
+
+This DAPP is an emulatin of craiglist and is build as a project of [this]{https://www.udemy.com/getting-started-with-ethereum-solidity-development/} course.  
+We hve used Ethereum block chain and Truffle to build this DAPP
--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,91 @@
 App = {
      web3Provider: null,
      contracts: {},
+     account: 0x0,
 
      init: function() {
-          /*
-           * Replace me...
-           */
 
           return App.initWeb3();
      },
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+          // initialize web3
+          if(typeof web3 !== 'undefined') {
+            // Resuse provider of web3 object injected by metamask
+            App.web3Provider = web3.currentProvider;
+          } else {
+            // create new provider and plug it into local node
+            App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
+          }
+
+          web3 = new Web3(App.web3Provider);
+
+          App.displayAccountInfo();
 
           return App.initContract();
      },
 
-     initContract: function() {
-          /*
-           * Replace me...
-           */
+     displayAccountInfo: function() {
+      web3.eth.getCoinbase(function(err, account) {
+            if(err === null) {
+                  App.account = account;
+                  $('#account').text(account);
+                  web3.eth.getBalance(account, function(err, balance) {
+                        if(err === null) {
+                              $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+                        }
+                  })
+            }
+      });
      },
+
+     initContract: function() {
+          $.getJSON('ChainList.json', function(ChainListArtifact) {
+                // get Contract artifact file and ise it to instantiate truffle contract abstraction
+                App.contracts.ChainList = TruffleContract(ChainListArtifact);
+                // set the provide for our contract
+                App.contracts.ChainList.setProvider(App.web3Provider);
+                // retrieve the article from the contract
+                return App.reloadArticles(); 
+          })
+     },
+
+     reloadArticles: function() {
+           // refersh account information because the balance might have changed
+           App.displayAccountInfo();
+
+           // retrieve the article placeholder and clear it
+           $('#articlesRow').empty();
+
+           App.contracts.ChainList.deployed().then(function(instance) {
+                 return instance.getArticle();
+           }).then(function(article) {
+                 if(article[0] == 0x0) {
+                       // no article to display
+                       return;
+                 }
+
+                 // Retrieve the article template and fill it with data
+
+                  // load articlesRow
+                  var articlesRow = $('#articlesRow');
+                  var articleTemplate = $('#articleTemplate');
+
+                  articleTemplate.find('.panel-title').text(article[1]);
+                  articleTemplate.find('.article-description').text(article[2]);
+                  articleTemplate.find('.article-price').text(web3.fromWei(article[3], "ether"));
+
+                  var seller = article[0];
+                  if (seller == App.account) {
+                        seller = "You";
+                  }
+                  articleTemplate.find('.article-seller').text(seller);
+
+                  articlesRow.append(articleTemplate.html());
+           }).catch(function(err) {
+                 console.log(err.message)
+           });
+     }
 };
 
 $(function() {
